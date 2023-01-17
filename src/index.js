3df3326350112c10fc3b1ce3aae61729035cad02
kipchumba-ket <kipchumbaelly97@gmail.com>
2023-01-17 10:01:14 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import Article from "./Article";
+import Comment from "./Comment";
 ReactDOM.render(<App />, document.getElementById("root"));
